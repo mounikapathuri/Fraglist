7f78ddbe2c3236f285f68299c82b19ca59e9b7aa
mounika <mounikapathuri2017@gmail.com>
2017-12-12 08:59:55 +0530

    Initial commit

---
+++ app/src/main/java/com/android/mounika/fraglist/Frag1.java
@@ -0,0 +1,46 @@
+package com.android.mounika.fraglist;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import static android.R.attr.button;
+
+
+public class Frag1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v= inflater.inflate(R.layout.fragment_frag1, container, false);
+        return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+
+    }
+
+
+}
---
+++ app/src/main/java/com/android/mounika/fraglist/Frag2.java
@@ -0,0 +1,10 @@
+package com.android.mounika.fraglist;
+
+import android.app.Fragment;
+
+/**
+ * Created by mounika on 16/06/2017.
+ */
+
+public class Frag2 extends Fragment {
+}
